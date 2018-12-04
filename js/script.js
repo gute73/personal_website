0e35d7a30673c263a7413304f4be1a5d2ab1b1a5
Mark D. King <kingmarkd@gmail.com>
2018-12-04 17:04:53 -0500

    Updated Resume and About Page

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ $(document).ready( function() {
 
 	$('#portfolio').on('mouseenter', '.portfoliobox', function() {
 		$(this).find('.foliotxt').show();
-		$(this).find('.folioimg').hide();
 	});
 
 	$('#portfolio').on('mouseleave', '.portfoliobox', function() {
 		$(this).find('.foliotxt').hide();
-		$(this).find('.folioimg').show();
 	});
+
 });
