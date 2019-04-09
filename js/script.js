edf08a868298a9f448e7a264354950a09ca2f02c
Mark D. King <kingmarkd@gmail.com>
2019-04-09 17:51:00 -0400

    Cleaning up some visual issues

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 $(document).ready( function() {
 	$('.foliotxt').hide();
+  $('.folioimg-dark').hide();
 
 	$('#portfolio').on('mouseenter', '.portfoliobox', function() {
+    $(this).find('.folioimg').hide();
 		$(this).find('.foliotxt').show();
+    $(this).find('.folioimg-dark').show();
 	});
 
 	$('#portfolio').on('mouseleave', '.portfoliobox', function() {
-		$(this).find('.foliotxt').hide();
+    $(this).find('.foliotxt').hide();
+    $(this).find('.folioimg-dark').hide();
+    $(this).find('.folioimg').show();
 	});
 
 });
